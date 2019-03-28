b363f7e5197c69ff58f2b2ffd03d09a8ee63b5fe
Logan Ziegler <skyofblue22@yahoo.com>
2019-03-27 22:14:25 -0400

    Added and deleted files for final version

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-Hello
+'use strict';	
+	
+(function($){
+	
+	// $("html, body").scrollTop($(element).offset().top);
+	
+	function navClick() {
+		$('a.jsNavLink').click(function(event) {
+			event.preventDefault();
+			let positionOne = $($(this).attr('href')).offset().top;
+			$('html, body').animate({
+				scrollTop: positionOne
+			}, 700);
+		});
+	};
+	
+	function backToTop() {
+		$('a.jsTopLink').click(function(event) {
+			event.preventDefault();
+			let positionTwo = $($(this).attr('href')).offset().top;
+			$('html, body').animate({
+				scrollTop: positionTwo
+			}, 700);
+		});
+	};
+	
+	navClick();
+	backToTop();
+	
+}(jQuery));
